285f0eaf676cbcf08bd89f8dbf131b0a4cfa755a
Salvador Juan Martinez <saljuama@gmail.com>
2020-05-22 01:02:37 +0200

    refactor behaviour for well aging products (aged brie)

--- src/main/kotlin/dev/saljuama/katas/refactor/gildedrose/GildedRose.kt
+++ src/main/kotlin/dev/saljuama/katas/refactor/gildedrose/GildedRose.kt
@@ -4,12 +4,20 @@ class GildedRose(var items: Array<Item>) {
 
     fun updateQuality() {
         val legendaryItems = listOf("Sulfuras, Hand of Ragnaros")
+        val wellAgingItems = listOf("Aged Brie")
 
         for (i in items.indices) {
             if (legendaryItems.contains(items[i].name))
                 continue
+            if (wellAgingItems.contains(items[i].name)) {
+                items[i].sellIn = items[i].sellIn - 1
+                if (items[i].quality < 50) {
+                    items[i].quality = items[i].quality + 1
+                }
+                continue
+            }
 
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
+            if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
                 if (items[i].quality > 0) {
                     items[i].quality = items[i].quality - 1
                 }
@@ -35,19 +43,15 @@ class GildedRose(var items: Array<Item>) {
 
             items[i].sellIn = items[i].sellIn - 1
             if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].quality > 0) {
-                            items[i].quality = items[i].quality - 1
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality
+
+                if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
+                    if (items[i].quality > 0) {
+                        items[i].quality = items[i].quality - 1
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
-                    }
+                    items[i].quality = items[i].quality - items[i].quality
                 }
+
             }
         }
     }
