9509b8c3c1bb1f85e93b1c0859f3d584aa4d2bed
Salvador Juan Martinez <saljuama@gmail.com>
2020-05-22 00:56:40 +0200

    refactor behaviour for legendary items

--- src/main/kotlin/dev/saljuama/katas/refactor/gildedrose/GildedRose.kt
+++ src/main/kotlin/dev/saljuama/katas/refactor/gildedrose/GildedRose.kt
@@ -3,12 +3,15 @@ package dev.saljuama.katas.refactor.gildedrose
 class GildedRose(var items: Array<Item>) {
 
     fun updateQuality() {
+        val legendaryItems = listOf("Sulfuras, Hand of Ragnaros")
+
         for (i in items.indices) {
+            if (legendaryItems.contains(items[i].name))
+                continue
+
             if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
                 if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        items[i].quality = items[i].quality - 1
-                    }
+                    items[i].quality = items[i].quality - 1
                 }
             } else {
                 if (items[i].quality < 50) {
@@ -30,17 +33,12 @@ class GildedRose(var items: Array<Item>) {
                 }
             }
 
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                items[i].sellIn = items[i].sellIn - 1
-            }
-
+            items[i].sellIn = items[i].sellIn - 1
             if (items[i].sellIn < 0) {
                 if (items[i].name != "Aged Brie") {
                     if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
                         if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                items[i].quality = items[i].quality - 1
-                            }
+                            items[i].quality = items[i].quality - 1
                         }
                     } else {
                         items[i].quality = items[i].quality - items[i].quality
