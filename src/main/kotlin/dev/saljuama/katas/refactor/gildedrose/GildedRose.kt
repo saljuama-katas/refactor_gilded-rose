0cacf463dda90e959180e42fa6df65588221b6fd
Salvador Juan Martinez <saljuama@gmail.com>
2020-05-22 01:26:21 +0200

    cleanup conditions and loop continue conditions

--- src/main/kotlin/dev/saljuama/katas/refactor/gildedrose/GildedRose.kt
+++ src/main/kotlin/dev/saljuama/katas/refactor/gildedrose/GildedRose.kt
@@ -3,36 +3,33 @@ package dev.saljuama.katas.refactor.gildedrose
 class GildedRose(var items: Array<Item>) {
 
     fun updateQuality() {
-        val legendaryItems = listOf("Sulfuras, Hand of Ragnaros")
-        val wellAgingItems = listOf("Aged Brie")
+        fun isLegendaryItem(item: String) = listOf("Sulfuras, Hand of Ragnaros").contains(item)
+        fun isWellAgingItem(item:String) = listOf("Aged Brie").contains(item)
+        fun isBackstagePass(item:String) = item.contains("Backstage passes")
 
         for (i in items.indices) {
-            if (legendaryItems.contains(items[i].name))
+            if (isLegendaryItem(items[i].name))
                 continue
-            if (wellAgingItems.contains(items[i].name)) {
-                items[i].sellIn -= 1
-                if (items[i].quality < 50) {
+
+            items[i].sellIn -= 1
+
+            if (isWellAgingItem(items[i].name)) {
+                if (items[i].quality < 50)
                     items[i].quality += 1
-                }
-                continue
             }
-            if (items[i].name.contains("Backstage passes")) {
-                items[i].sellIn -= 1
+            else if (isBackstagePass(items[i].name)) {
                 when {
                     items[i].sellIn > 10 -> items[i].quality += 1
                     items[i].sellIn in 6..10 -> items[i].quality += 2
                     items[i].sellIn in 0..5 -> items[i].quality += 3
                     else -> items[i].quality = 0
                 }
-                continue
             }
-
-            items[i].sellIn = items[i].sellIn - 1
-            if (items[i].quality > 0) {
-                items[i].quality -= 1
-            }
-            if (items[i].sellIn < 0 && items[i].quality > 0) {
-                items[i].quality -= 1
+            else {
+                if (items[i].quality > 0)
+                    items[i].quality -= 1
+                if (items[i].sellIn < 0 && items[i].quality > 0)
+                    items[i].quality -= 1
             }
         }
     }
