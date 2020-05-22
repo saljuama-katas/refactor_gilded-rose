b8769a2c1862da08ef3858f8803fc053ac9c75c3
Salvador Juan Martinez <saljuama@gmail.com>
2020-05-22 02:01:29 +0200

    extract the behaviour of different items into different classes

--- src/main/kotlin/dev/saljuama/katas/refactor/gildedrose/GildedRose.kt
+++ src/main/kotlin/dev/saljuama/katas/refactor/gildedrose/GildedRose.kt
@@ -3,35 +3,10 @@ package dev.saljuama.katas.refactor.gildedrose
 class GildedRose(var items: Array<Item>) {
 
     fun updateQuality() {
-        fun isLegendaryItem(item: String) = listOf("Sulfuras, Hand of Ragnaros").contains(item)
-        fun isWellAgingItem(item:String) = listOf("Aged Brie").contains(item)
-        fun isBackstagePass(item:String) = item.contains("Backstage passes")
-
-        for (i in items.indices) {
-            if (isLegendaryItem(items[i].name))
-                continue
-
-            items[i].sellIn -= 1
-
-            if (isWellAgingItem(items[i].name)) {
-                if (items[i].quality < 50)
-                    items[i].quality += 1
-            }
-            else if (isBackstagePass(items[i].name)) {
-                when {
-                    items[i].sellIn > 10 -> items[i].quality += 1
-                    items[i].sellIn in 6..10 -> items[i].quality += 2
-                    items[i].sellIn in 0..5 -> items[i].quality += 3
-                    else -> items[i].quality = 0
-                }
-            }
-            else {
-                if (items[i].quality > 0)
-                    items[i].quality -= 1
-                if (items[i].sellIn < 0 && items[i].quality > 0)
-                    items[i].quality -= 1
-            }
-        }
+        items = items
+            .map { itemFactory(it) }
+            .map { it.updateQuality() }
+            .toTypedArray()
     }
 
 }
--- src/main/kotlin/dev/saljuama/katas/refactor/gildedrose/Item.kt
+++ src/main/kotlin/dev/saljuama/katas/refactor/gildedrose/Item.kt
@@ -5,3 +5,50 @@ open class Item(var name: String, var sellIn: Int, var quality: Int) {
         return this.name + ", " + this.sellIn + ", " + this.quality
     }
 }
+
+interface SmarterItem {
+    fun updateQuality(): Item
+}
+
+fun itemFactory(item: Item): SmarterItem {
+    fun isLegendaryItem(item: String) = listOf("Sulfuras, Hand of Ragnaros").contains(item)
+    fun isWellAgingItem(item:String) = listOf("Aged Brie").contains(item)
+    fun isBackstagePass(item:String) = item.contains("Backstage passes")
+
+    if (isLegendaryItem(item.name))
+        return LegendaryItem(item.name, item.sellIn, item.quality)
+    if (isWellAgingItem(item.name))
+        return WellAgingItem(item.name, item.sellIn, item.quality)
+    if (isBackstagePass(item.name))
+        return BackstagePassesItem(item.name, item.sellIn, item.quality)
+    return RegularItem(item.name, item.sellIn, item.quality)
+}
+
+class LegendaryItem(val name: String, val sellIn: Int, val quality: Int) : SmarterItem {
+    override fun updateQuality(): Item = Item(name, sellIn, quality)
+}
+
+class WellAgingItem(val name: String, val sellIn: Int, val quality: Int) : SmarterItem {
+    override fun updateQuality(): Item = Item(name, sellIn - 1, when {
+        quality < 50 -> quality + 1
+        else -> quality
+    })
+}
+
+class BackstagePassesItem(val name: String, val sellIn: Int, val quality: Int) : SmarterItem {
+    override fun updateQuality(): Item = Item(name, sellIn - 1, when {
+        sellIn - 1 > 10 -> quality + 1
+        sellIn - 1 in 6..10 -> quality + 2
+        sellIn - 1 in 0..5 -> quality + 3
+        else ->  0
+    })
+}
+
+class RegularItem(val name: String, val sellIn: Int, val quality: Int) : SmarterItem {
+    override fun updateQuality(): Item {
+        var newQuality = quality
+        if (quality > 0 ) newQuality -= 1
+        if (sellIn - 1 < 0 && newQuality > 0 ) newQuality -= 1
+        return Item(name, sellIn - 1, newQuality)
+    }
+}
