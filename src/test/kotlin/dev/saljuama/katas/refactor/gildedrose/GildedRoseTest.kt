99e5b4323334ca902858101d1b3b621f57864c40
Salvador Juan Martinez <saljuama@gmail.com>
2020-05-24 14:15:03 +0200

    refactor tests to extract code duplication

--- src/test/kotlin/dev/saljuama/katas/refactor/gildedrose/GildedRoseTest.kt
+++ src/test/kotlin/dev/saljuama/katas/refactor/gildedrose/GildedRoseTest.kt
@@ -4,94 +4,84 @@ import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
 
 class GildedRoseTest : FreeSpec({
-
     "The Gilded Rose Inn" - {
 
-        "updating the quality of the items" - {
+        "when updating the quality of the items" - {
+            fun updateQuality(item: Item): Item {
+                val inventory = GildedRose(arrayOf(item))
+                inventory.updateQuality()
+                return inventory.items.first()
+            }
 
             "for regular items" - {
                 "degrades sellIn and the quality values by 1" {
-                    val inventory = GildedRose(arrayOf(Item("foo", 10, 10)))
-                    inventory.updateQuality()
-                    inventory.items.first().sellIn shouldBe 9
-                    inventory.items.first().quality shouldBe 9
-
+                    val updatedItem = updateQuality(Item("foo", 10, 10))
+                    updatedItem.sellIn shouldBe 9
+                    updatedItem.quality shouldBe 9
                 }
                 "never degrades quality below 0" {
-                    val inventory = GildedRose(arrayOf(Item("foo", 0, 0)))
-                    inventory.updateQuality()
-                    inventory.items.first().quality shouldBe 0
+                    val updatedItem = updateQuality(Item("foo", 0, 0))
+                    updatedItem.quality shouldBe 0
                 }
                 "degrades the quality by 2 when the sellIn value is 0 or less" {
-                    val inventory = GildedRose(arrayOf(Item("foo", 0, 10)))
-                    inventory.updateQuality()
-                    inventory.items.first().quality shouldBe 8
+                    val updatedItem = updateQuality(Item("foo", 0, 10))
+                    updatedItem.quality shouldBe 8
                 }
             }
 
             "for items that age well" - {
-                "increase their quality over time" {
-                    val inventory = GildedRose(arrayOf(Item("Aged Brie", 10, 10)))
-                    inventory.updateQuality()
-                    inventory.items.first().quality shouldBe 11
+                "increase their quality by 1" {
+                    val updatedItem = updateQuality(Item("Aged Brie", 10, 10))
+                    updatedItem.quality shouldBe 11
                 }
-                "quality never goes above 50" {
-                    val inventory = GildedRose(arrayOf(Item("Aged Brie", 10, 50)))
-                    inventory.updateQuality()
-                    inventory.items.first().quality shouldBe 50
+                "quality never improves above 50" {
+                    val updatedItem = updateQuality(Item("Aged Brie", 10, 50))
+                    updatedItem.quality shouldBe 50
                 }
             }
 
             "for backstage passes" - {
                 "increase quality by 1 when sellIn is more than 10 days" {
-                    val inventory = GildedRose(arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", 15, 10)))
-                    inventory.updateQuality()
-                    inventory.items.first().quality shouldBe 11
+                    val updatedItem = updateQuality(Item("Backstage passes to a TAFKAL80ETC concert", 15, 10))
+                    updatedItem.quality shouldBe 11
                 }
                 "increase quality by 2 when sellIn is 10 days or less" {
-                    val inventory = GildedRose(arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", 10, 10)))
-                    inventory.updateQuality()
-                    inventory.items.first().quality shouldBe 12
+                    val updatedItem = updateQuality(Item("Backstage passes to a TAFKAL80ETC concert", 10, 10))
+                    updatedItem.quality shouldBe 12
                 }
                 "increase quality by 3 when sellIn is 5 days or less" {
-                    val inventory = GildedRose(arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", 5, 10)))
-                    inventory.updateQuality()
-                    inventory.items.first().quality shouldBe 13
+                    val updatedItem = updateQuality(Item("Backstage passes to a TAFKAL80ETC concert", 5, 10))
+                    updatedItem.quality shouldBe 13
                 }
                 "drop the quality to 0 the day after the concert" {
-                    val inventory = GildedRose(arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", 0, 10)))
-                    inventory.updateQuality()
-                    inventory.items.first().quality shouldBe 0
+                    val updatedItem = updateQuality(Item("Backstage passes to a TAFKAL80ETC concert", 0, 10))
+                    updatedItem.quality shouldBe 0
                 }
             }
 
             "for legendary items" - {
                 "never change their sellIn or quality attributes" {
-                    val inventory = GildedRose(arrayOf(Item("Sulfuras, Hand of Ragnaros", 10, 10)))
-                    inventory.updateQuality()
-                    inventory.items.first().sellIn shouldBe 10
-                    inventory.items.first().quality shouldBe 10
+                    val updatedItem = updateQuality(Item("Sulfuras, Hand of Ragnaros", 10, 10))
+                    updatedItem.sellIn shouldBe 10
+                    updatedItem.quality shouldBe 10
                 }
             }
 
             "for conjured items" - {
-                "degrades quality by 2" {
-                    val inventory = GildedRose(arrayOf(Item("Conjured foo", 10, 10)))
-                    inventory.updateQuality()
-                    inventory.items.first().quality shouldBe 8
+                "degrades sellIn by 1 and quality by 2" {
+                    val updatedItem = updateQuality(Item("Conjured foo", 10, 10))
+                    updatedItem.sellIn shouldBe 9
+                    updatedItem.quality shouldBe 8
                 }
                 "degrades the quality by 4 when the sellIn is 0 or less" {
-                    val inventory = GildedRose(arrayOf(Item("Conjured foo", 0, 10)))
-                    inventory.updateQuality()
-                    inventory.items.first().quality shouldBe 6
+                    val updatedItem = updateQuality(Item("Conjured foo", 0, 10))
+                    updatedItem.quality shouldBe 6
                 }
                 "never degrades quality below 0" {
-                    val inventory = GildedRose(arrayOf(Item("Conjured foo", 0, 3)))
-                    inventory.updateQuality()
-                    inventory.items.first().quality shouldBe 0
+                    val updatedItem = updateQuality(Item("Conjured foo", 0, 3))
+                    updatedItem.quality shouldBe 0
                 }
             }
         }
     }
 })
-
